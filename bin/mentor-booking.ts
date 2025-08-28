975bd2af1d307d487b408784e10a2056b97bcab5
Maksym Ivanchenko <ivanchenko.maxim2000@gmail.com>
2025-08-28 21:28:00 +0300

    Create DynamoDB tables

--- bin/mentor-booking.ts
+++ bin/mentor-booking.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { DynamoDbStack } from '../lib/dynamodb/dynamodb-stack';
 
 const app = new cdk.App();
+new DynamoDbStack(app, 'DynamoDbStack');
